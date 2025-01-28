b48ed1db1962b957d9f41c2f561a4e1b8911600a
erfanili <er.esmaili2060@gmail.com>
2025-01-28 08:53:27 -0500

    first commit

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/user-data', (req, res) => {
 });
 
 // Start the server on port 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
